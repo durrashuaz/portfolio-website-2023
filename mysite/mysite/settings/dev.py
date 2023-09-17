f56f7fdae2cdf11aaaf708fae70f869963bdc917
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2023-09-17 16:20:48 +0100

    Remove .pyc files from tracking

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xcg$m@6u!xydwkmj743!l1jnrg5^46g&wg4rn$c6oq2!b#3q#n"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
